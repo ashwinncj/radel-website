8e6eaf18ec26b316d69b2a484edb8a8762bc4237
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2019-01-31 16:53:33 +0000

    Update

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>RADEL Nav</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="assets/css/expand.css">
+        <script src="assets/js/expand.js"></script>
+    </head>
+    <body>
+        <div class="radelmenu">
+            <div class="radel-menu-btn"><img class="nav-img" src="assets/img/icons8-menu-500.png"></div>
+            <div class="radel-menu-close-control"></div>
+            <div class="menu-items">
+                <ul>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-start-menu-100.png"></span><span class="menu-title">Home</span></a></li>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-company-filled-100.png"></span><span class="menu-title">About</span></a></li>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-e-learning-100.png"></span><span class="menu-title">Resources</span></a></li>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-download-from-cloud-100.png"></span><span class="menu-title">Downloads</span></a></li>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-phone-100.png"></span><span class="menu-title">Contact</span></a></li>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-e-learning-100.png"></span><span class="menu-title">Resources</span></a></li>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-download-from-cloud-100.png"></span><span class="menu-title">Downloads</span></a></li>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-company-filled-100.png"></span><span class="menu-title">About</span></a></li>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-e-learning-100.png"></span><span class="menu-title">Resources</span></a></li>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-download-from-cloud-100.png"></span><span class="menu-title">Downloads</span></a></li>
+                    <li><a href="#"><span class="menu-link-img"><img class="nav-img" src="assets/img/icons8-phone-100.png"></span><span class="menu-title">Contact</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
